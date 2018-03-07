520e8b263d69bdc9b60738ddcb0e440c150fb7bc
Rafael Corsi <corsiferrao@gmail.com>
2018-03-07 19:28:18 -0300

    organizando pastas

--- C-LogicaCombinacional/src/rtl/Not16.vhd
+++
@@ -1,8 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-entity Not16 is
-	port ( 
-			a:   in  STD_LOGIC_VECTOR(15 downto 0);
-			q:   out STD_LOGIC_VECTOR(15 downto 0));
-end entity;
---
+++ Projetos/C-LogicaCombinacional/src/rtl/Or8Way.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Or8Way is
+	port ( 
+			a:   in  STD_LOGIC;
+			b:   in  STD_LOGIC;
+			c:   in  STD_LOGIC;
+			d:   in  STD_LOGIC;
+			e:   in  STD_LOGIC;
+			f:   in  STD_LOGIC;
+			g:   in  STD_LOGIC;
+			h:   in  STD_LOGIC;
+			q:   out STD_LOGIC);
+end entity;
