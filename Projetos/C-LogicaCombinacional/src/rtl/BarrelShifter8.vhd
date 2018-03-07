4b6bdb8572c4b83ae0b49c2a31ac643571ccc85b
Rafael Corsi <corsiferrao@gmail.com>
2018-03-07 19:28:18 -0300

    organizando pastas

---
+++ Projetos/C-LogicaCombinacional/src/rtl/BarrelShifter8.vhd
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity BarrelShifter8 is
+	port ( 
+			a:    in  STD_LOGIC_VECTOR(7 downto 0);   -- input vector
+			dir:  in  std_logic;                       -- 0=>left 1=>right
+			size: in  STD_LOGIC_VECTOR(2 downto 0);    -- shift amount
+			q:    out STD_LOGIC_VECTOR(7 downto 0));  -- output vector (shifted)
+end entity;
---
+++ Projetos/C-LogicaCombinacional/src/rtl/Not16.vhd
@@ -0,0 +1,8 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Not16 is
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end entity;
