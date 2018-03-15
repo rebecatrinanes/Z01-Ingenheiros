9d4a952cf4660891eb4563ada4de5b36c4d481f5
Rafael Corsi <corsiferrao@gmail.com>
2018-03-15 17:33:32 -0300

    fix nand test

--- Projetos/C-LogicaCombinacional/tests/tst/tb_nand.vhd
+++ Projetos/C-LogicaCombinacional/tests/tst/tb_nand.vhd
@@ -15,7 +15,7 @@ end entity;
 
 architecture tb of tb_nand is
 
-  component nand_z01 is
+  component nand_vhdl is
     port(a:in  STD_LOGIC;
          b:in  STD_LOGIC;
          q:out STD_LOGIC
@@ -30,15 +30,7 @@ architecture tb of tb_nand is
 
 begin
 
-  clk_process :process
-  begin
-    clk <= '0';
-    wait for clk_period/2;  --for 0.5 ns signal is '0'.
-    clk <= '1';
-    wait for clk_period/2;  --for next 0.5 ns signal is '1'.
-  end process;
-
-  mapping: nand_z01 port map(inA, inB, outQ);
+  mapping: nand_vhdl port map(inA, inB, outQ);
 
   main : process
   begin
@@ -46,22 +38,22 @@ begin
 
     -- Teste: 0 0
     inA <= '0'; inB <= '0';
-    wait until clk='1';
+    wait for 200ps;
     assert(outQ = '1')  report "Falha em teste: 0 nand 0 != 1" severity error;
 
     -- Teste: 0 1
     inA <= '0'; inB <= '1';
-    wait until clk='1';
+    wait for 200ps;
     assert(outQ = '1')  report "Falha em teste: 0 nand 1 != 1" severity error;
 
     -- Teste: 1 0
     inA <= '1'; inB <= '0';
-    wait until clk='1';
+    wait for 200ps;
     assert(outQ = '1')  report "Falha em teste: 1 nand 0 != 1" severity error;
 
     -- Teste: 1 1
     inA <= '1'; inB <= '1';
-    wait until clk='1';
+    wait for 200ps;
     assert(outQ = '0')  report "Falha em teste: 1 nand 1 != 0" severity error;
 
     test_runner_cleanup(runner); -- Simulation ends here
