b3abb0e1f881c9c0f90cfcbbe652a5195958ba73
Rafael Corsi <corsiferrao@gmail.com>
2018-03-15 18:27:25 -0300

    projeto D

---
+++ Projetos/D-UnidadeLogicaAritmetica/src/rtl/zerador16.vhd
@@ -0,0 +1,20 @@
+-- Elementos de Sistemas
+-- by Luciano Soares
+-- zerador16.vhd
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity zerador16 is
+   port(z   : in STD_LOGIC;
+	      a   : in STD_LOGIC_VECTOR(15 downto 0);
+        y   : out STD_LOGIC_VECTOR(15 downto 0)
+   );
+end zerador16;
+
+architecture rtl of zerador16 is
+
+
+begin
+
+end architecture;
