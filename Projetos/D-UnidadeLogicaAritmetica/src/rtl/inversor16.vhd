8ad7d3a96287439b82989231cbb011213fc2f1a4
Veguinho <rafaelvr@al.insper.edu.br>
2018-03-21 00:28:12 -0300

    inversor

--- Projetos/D-UnidadeLogicaAritmetica/src/rtl/inversor16.vhd
+++ Projetos/D-UnidadeLogicaAritmetica/src/rtl/inversor16.vhd
@@ -7,13 +7,17 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity inversor16 is
    port(z   : in STD_LOGIC;
-	     a   : in STD_LOGIC_VECTOR(15 downto 0);
+	a   : in STD_LOGIC_VECTOR(15 downto 0);
         y   : out STD_LOGIC_VECTOR(15 downto 0)
    );
 end entity;
 
 architecture rtl of inversor16 is
-
 begin
-
+process(a,y,z)
+begin
+	if (z = '0') then y <= a;
+	else y <= not a;
+	end if;
+end process;
 end architecture;
