7f45e7387ef7c44b9009c0bd9137177fd958961e
Rafael Corsi <corsiferrao@gmail.com>
2018-03-21 12:06:41 -0300

    projeto E - Logica Sequencial

---
+++ Projetos/E-LogicaSequencial/src/rtl/Register8.vhd
@@ -0,0 +1,15 @@
+-- Elementos de Sistemas
+-- by Luciano Soares
+-- Register8.vhd
+
+Library ieee; 
+use ieee.std_logic_1164.all;
+  
+entity Register8 is
+	port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC_VECTOR(7 downto 0);
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC_VECTOR(7 downto 0)
+	);
+end entity;
