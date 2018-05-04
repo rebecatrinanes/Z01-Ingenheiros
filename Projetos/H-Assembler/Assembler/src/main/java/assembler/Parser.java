052fff593c0b86e63d7c1b3ece800ed48ab9b121
TeraByte900 <fe.n.teracini@hotmail.com>
2018-05-04 11:12:19 -0300

    Parser ready to go

--- Projetos/H-Assembler/Assembler/src/main/java/assembler/Parser.java
+++ Projetos/H-Assembler/Assembler/src/main/java/assembler/Parser.java
@@ -4,14 +4,19 @@
  */
 
 package assembler;
-
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 /**
  * Encapsula o código de leitura. Carrega as instruções na linguagem assembly,
  * analisa, e oferece acesso as partes da instrução  (campos e símbolos).
  * Além disso, remove todos os espaços em branco e comentários.
  */
 public class Parser {
-
+	
+	public Scanner scanner;
+	public String line;
+	
     /** Enumerator para os tipos de comandos do Assembler. */
     public enum CommandType {
         A_COMMAND,      // comandos LEA, que armazenam no registrador A
@@ -24,7 +29,17 @@ public class Parser {
      * @param file arquivo NASM que será feito o parser.
      */
     public Parser(String file) {
-
+    	
+    	this.line = new String();
+    	
+    	try {
+    	    File arquivo = new File(file);
+    		scanner = new Scanner(arquivo);
+    	}
+    	catch (FileNotFoundException e){
+    		System.out.println("Arquivo n�o encontrado - Erro");
+			e.printStackTrace();
+    	}
     }
 
     /**
@@ -34,7 +49,22 @@ public class Parser {
      * @return Verdadeiro se ainda há instruções, Falso se as instruções terminaram.
      */
     public Boolean advance() {
-    	return null;
+    	if (scanner.hasNext()){
+    		String helper = scanner.nextLine();
+    		helper = helper.trim();
+    		while(scanner.hasNext() && (helper.equals("") || helper.equals("\n") || helper.startsWith(";"))){
+    			helper = scanner.nextLine();
+    			helper = helper.trim();
+    		}
+    		line = helper;
+    		line = line.replaceAll("\\s+", " ");
+    		if(line.contains(";")){
+    			line = line.substring(0, line.indexOf(';'));
+    			line = line.trim();
+    		}
+    		return true;
+    	}
+    	return false;
     }
 
     /**
@@ -42,7 +72,7 @@ public class Parser {
      * @return a instrução atual para ser analilisada
      */
     public String command() {
-    	return null;
+    	return this.line;
     }
 
     /**
@@ -53,8 +83,17 @@ public class Parser {
      * @param  command instrução a ser analisada.
      * @return o tipo da instrução.
      */
-    public CommandType commandType(String command) {
-    	return null;
+    public CommandType commandType(String command) {	
+    	if (command.endsWith(":")){
+    		return CommandType.L_COMMAND;
+    	}
+    	if (command.startsWith("leaw")){
+    		return CommandType.A_COMMAND;
+    	}
+    	else{
+    		return CommandType.C_COMMAND;
+    	}
+
     }
 
     /**
@@ -63,9 +102,18 @@ public class Parser {
      * @param  command instrução a ser analisada.
      * @return somente o símbolo ou o valor número da instrução.
      */
+    // leaw $TESTE, %A
+
     public String symbol(String command) {
-    	return null;
+    	String symbol;
+    	int cifr;
+    	int virg;
+    	cifr = command.indexOf("$")+1;
+    	virg = command.indexOf(",");
+    	symbol = command.substring(cifr, virg);
+    	return symbol;
     }
+    
 
     /**
      * Retorna o símbolo da instrução passada no argumento.
@@ -74,7 +122,10 @@ public class Parser {
      * @return o símbolo da instrução (sem os dois pontos).
      */
     public String label(String command) {
-    	return null;
+    	String symbol;
+		symbol = command.substring(0, command.length()-1);
+		return symbol;
+    	
     }
 
     /**
@@ -84,7 +135,12 @@ public class Parser {
      * @return um vetor de string contento os tokens da instrução (as partes do comando).
      */
     public String[] instruction(String command) {
-    	return null;
+    	// MOVW %A, %D
+    	// JMP %D
+		String[] vector;
+    	command = command.replace(",", " ");
+		vector = command.split(" ");
+		return vector;
     }
 
 }
